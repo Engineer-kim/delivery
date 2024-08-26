c8174a4fd2be2d454c2940f5f32b5a1ede0510b2
suin <suin505@gmail.com>
2024-08-26 16:16:56 +0900

    Audit 수정, User 서치 기능 추가

--- src/main/java/com/sparta/delivery/common/AuditorAwareImpl.java
+++ src/main/java/com/sparta/delivery/common/AuditorAwareImpl.java
@@ -1,12 +1,9 @@
 package com.sparta.delivery.common;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.lang.NonNull;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
