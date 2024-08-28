6025a6ffaf6170d61861ba8662b389ce471bb8a9
Kimhanjin <ddang0103@naver.com>
2024-08-28 15:12:27 +0900

    Feature: 리뷰 도메인애 댜헌 전체  API 개발 완료

--- src/main/java/com/sparta/delivery/review/entity/Review.java
+++ src/main/java/com/sparta/delivery/review/entity/Review.java
@@ -1,48 +1,60 @@
 package com.sparta.delivery.review.entity;
 
+import com.sparta.delivery.common.TimeStamped;
 import com.sparta.delivery.common.statusEnum.DataStatus;
 import com.sparta.delivery.common.statusEnum.PrivacyStatus;
+import com.sparta.delivery.user.User;
 import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 @Entity
 @Table(name = "p_review")
-public class Review {
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Setter
+public class Review extends TimeStamped {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "review_id")
-    private Integer reviewId;
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID reviewId;
 
-    /**리뷰 내용*/
+    //리뷰 내용
     @Column(name = "review_content", length = 150, nullable = false)
     private String reviewContent;
 
-    /**리뷰 별점*/
+    //리뷰 제목
+    @Column(name = "review_title", length = 150, nullable = false)
+    private String reviewTitle;
+
+    //리뷰 별점
     @Column(name = "review_rating", precision = 2, scale = 1, nullable = false)
     private BigDecimal reviewRating;
 
-    /**메뉴 아이디*/
-    @Column(name = "menu_id")
-    private Integer menuId;
-
-    /**삭제 여부 판단*/
+    //삭제 여부 판단
     @Enumerated(EnumType.STRING)
     @Column(name = "db_sts",nullable = false , columnDefinition = "VARCHAR(1)")
     private DataStatus deleteStatus;
 
-    /**공개 여부*/
+    //공개 여부
     @Enumerated(EnumType.STRING)
     @Column(name = "public_sts", nullable = false, columnDefinition = "VARCHAR(1)")
     private PrivacyStatus privacyStatus;
 
-    /**사용자 아이디*/
+    //사용자 아이디
     @Column(name = "user_id")
-    private String userId;
+    private Long userId;
 
-    @Column(name = "db_sts", length = 1, nullable = false)
-    private String dbStatus;
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false, insertable = false, updatable = false)
+    private User user;
 
     @PrePersist
     private void setDefaultStatus() {
