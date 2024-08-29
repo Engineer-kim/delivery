03c79c910b3135775268e05a189297cee73d45cc
Mcgeolypazun <taratula0504@gmail.com>
2024-08-29 14:50:39 +0900

    코드 배열 및 import 정리

--- src/main/java/com/sparta/delivery/product/Product.java
+++ src/main/java/com/sparta/delivery/product/Product.java
@@ -1,26 +1,28 @@
 package com.sparta.delivery.product;
 
-import com.sparta.delivery.order.Order;
-import com.sparta.delivery.order.OrderItem;
 import com.sparta.delivery.shop.entity.Store;
-import jakarta.persistence.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.Fetch;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "products")
+@Table(name = "p_products")
 public class Product {
 
     @Id
@@ -42,7 +44,7 @@ public class Product {
     @Column(nullable = false)
     private boolean isDeleted = false;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
+    //    @ManyToOne(fetch = FetchType.LAZY)
 //    @JoinColumn(name = "order_id")
 //    private Order order;
     @Builder
--- src/main/java/com/sparta/delivery/shop/controller/ShopController.java
+++ src/main/java/com/sparta/delivery/shop/controller/ShopController.java
@@ -7,25 +7,24 @@ import com.sparta.delivery.security.UserDetailsImpl;
 import com.sparta.delivery.shop.dto.ShopData;
 import com.sparta.delivery.shop.dto.ShopRequest;
 import com.sparta.delivery.shop.dto.ShopResponse;
-import com.sparta.delivery.shop.entity.Store;
-import com.sparta.delivery.shop.exception.StoreException;
 import com.sparta.delivery.shop.service.ShopService;
-import com.sparta.delivery.user.User;
-import com.sparta.delivery.user.UserRoleEnum;
 import com.sparta.delivery.user.UserService;
-import com.sparta.delivery.user.dto.UserInfoDto;
-import jakarta.persistence.EntityNotFoundException;
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.CopyOnWriteArrayList;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -36,7 +35,9 @@ public class ShopController {
     private final UserService userService;
     private final ProductService productService;
 
-    /**거게 정보 추가*/
+    /**
+     * 거게 정보 추가
+     */
     @PostMapping("/shops")
     public ResponseEntity<ApiResponse> addStore(@RequestBody ShopRequest shopRequest,
         @AuthenticationPrincipal UserDetailsImpl userDetails) {
@@ -65,95 +66,111 @@ public class ShopController {
     }
 
 
-    /**가게 정보 전체 조회*/
+    /**
+     * 가게 정보 전체 조회
+     */
     @GetMapping("/shops")
-    public ResponseEntity<ApiResponse> getAllShops(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public ResponseEntity<ApiResponse> getAllShops(
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
         try {
             Long userId = userDetails.getUser().getId();
             List<ShopData> shopDataList = storeService.getAllShops(userId);
             ApiResponse response = new ApiResponse(
-                    200,
-                    "success",
-                    null,
-                    shopDataList.isEmpty() ? Collections.emptyList() : shopDataList
+                200,
+                "success",
+                null,
+                shopDataList.isEmpty() ? Collections.emptyList() : shopDataList
             );
 
             return ResponseEntity.ok(response);
         } catch (Exception e) {
             ApiResponse errorResponse = new ApiResponse(
-                    500,
-                    "fail",
-                    "가게 전체 조회 도중 오류가 발생했습니다",
-                    null
+                500,
+                "fail",
+                "가게 전체 조회 도중 오류가 발생했습니다",
+                null
             );
 
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
         }
     }
 
-    /**가게 정보 상세 조회*/
+    /**
+     * 가게 정보 상세 조회
+     */
     @GetMapping("/shops/{id}")
-    public ResponseEntity<ApiResponse> getOneShop(@AuthenticationPrincipal UserDetailsImpl userDetails ,@PathVariable UUID id) {
+    public ResponseEntity<ApiResponse> getOneShop(
+        @AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable UUID id) {
         try {
             Long userId = userDetails.getUser().getId();
-            ShopData findOne = storeService.getOneShop(userId , id);
+            ShopData findOne = storeService.getOneShop(userId, id);
             ApiResponse response = new ApiResponse(
-                    200,
-                    "success",
-                    null,
-                    findOne
+                200,
+                "success",
+                null,
+                findOne
             );
 
             return ResponseEntity.ok(response);
         } catch (Exception e) {
             ApiResponse errorResponse = new ApiResponse(
-                    500,
-                    "fail",
-                    "가게 단건 조회 도중 오류가 발생했습니다",
-                    null
+                500,
+                "fail",
+                "가게 단건 조회 도중 오류가 발생했습니다",
+                null
             );
 
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
         }
     }
 
-    /**가게 정보 가게 정보 수정*/
+    /**
+     * 가게 정보 가게 정보 수정
+     */
     @PutMapping("/shops/{id}")
-    public ResponseEntity<ApiResponse> updateShopInfo(@AuthenticationPrincipal UserDetailsImpl userDetails ,@PathVariable UUID id,
-                                                      @RequestBody ShopRequest updateRequest) {
+    public ResponseEntity<ApiResponse> updateShopInfo(
+        @AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable UUID id,
+        @RequestBody ShopRequest updateRequest) {
         try {
             Long userId = userDetails.getUser().getId();
-            ShopData updatedShopInfo = storeService.updateShopInfo(userId , id, updateRequest);
+            ShopData updatedShopInfo = storeService.updateShopInfo(userId, id, updateRequest);
             ApiResponse response = new ApiResponse(
-                    200,
-                    "success",
-                    null,
-                    updatedShopInfo
+                200,
+                "success",
+                null,
+                updatedShopInfo
             );
 
             return ResponseEntity.ok(response);
         } catch (Exception e) {
             ApiResponse errorResponse = new ApiResponse(
-                    500,
-                    "fail",
-                    "가게 수정 도중 오류가 발생했습니다"  + e.getMessage(),
-                    null
+                500,
+                "fail",
+                "가게 수정 도중 오류가 발생했습니다" + e.getMessage(),
+                null
             );
 
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
         }
     }
 
-    /**가게 정보 삭제*/
+    /**
+     * 가게 정보 삭제
+     */
     @DeleteMapping("/shops/{id}")
-    public ResponseEntity<ApiResponse> deleteShop(@PathVariable UUID id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-            Long userId = userDetails.getUser().getId();
-            ApiResponse response =  storeService.deleteShop(id ,userId);
-            return ResponseEntity.ok(response);
+    public ResponseEntity<ApiResponse> deleteShop(@PathVariable UUID id,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        Long userId = userDetails.getUser().getId();
+        ApiResponse response = storeService.deleteShop(id, userId);
+        return ResponseEntity.ok(response);
     }
-    /**가게 비공게 처리*/
+
+    /**
+     * 가게 비공게 처리
+     */
     @DeleteMapping("/shops/{id}/privacy")
-    public ResponseEntity<ApiResponse> makePrivateShop(@PathVariable UUID id,@AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public ResponseEntity<ApiResponse> makePrivateShop(@PathVariable UUID id,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
         Long userId = userDetails.getUser().getId();
         ApiResponse response = storeService.makePrivateShop(id, userId);
         return ResponseEntity.ok(response);
